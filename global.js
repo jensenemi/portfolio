e2a9c3fc7a4451ade69ab93aa21193eeb131c414
jensenemi <jeemi@ucsd.edu>
2025-10-14 14:17:35 -0700

    Update global.js

--- global.js
+++ global.js
@@ -32,7 +32,16 @@ for (let p of pages) {
     let url = p.url;
     let title = p.title;
     // next step: create link and add it to nav
-    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+      }
+    if (a.host !== location.host) {
+        a.target = '_blank';
+      }
   }
 
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")

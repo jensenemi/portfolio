3e87b8213fef0dc542560d8450924355c4f4d852
jensenemi <jeemi@ucsd.edu>
2025-10-14 15:00:14 -0700

    Update global.js

--- global.js
+++ global.js
@@ -41,7 +41,10 @@ for (let p of pages) {
     if (!url.startsWith('http')) {
         url = !url.startsWith('http') ? BASE_PATH + url : url;
       }
-    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
   }
 
   

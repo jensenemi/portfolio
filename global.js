5f3c8d03cc092618eb7b777c0818aab368f200c5
jensenemi <jeemi@ucsd.edu>
2025-10-14 13:44:12 -0700

    Update global.js

--- global.js
+++ global.js
@@ -4,15 +4,33 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-const navLinks = $$('nav a')
-navLinks.forEach(link => {
-    link.classList.remove('current');
-});
+// const navLinks = $$('nav a')
+// navLinks.forEach(link => {
+//     link.classList.remove('current');
+// });
 
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname,
-  );
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname,
+//   );
 
-if (currentLink) {
-    currentLink.classList.add('current');
-}
+// if (currentLink) {
+//     currentLink.classList.add('current');
+// }
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'resume/', title: 'Resume' },
+    { url: 'https://github.com/jensenemi', title: 'Github' }
+  ];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    // next step: create link and add it to nav
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+  }

3a6d0b333a7039eaf1c1e995accf2246f1d95a8d
jensenemi <jeemi@ucsd.edu>
2025-10-14 15:52:58 -0700

    Update global.js

--- global.js
+++ global.js
@@ -68,5 +68,6 @@ for (let p of pages) {
 const select = document.querySelector('.color-scheme select');
 select.addEventListener('input', function (event) {
     console.log('color scheme changed to', event.target.value);
+    document.documentElement.style.setProperty('color-scheme', event.target.value);
   });
   

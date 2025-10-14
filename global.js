6c4d2a57a797d66fe5605b034112dac614b553f3
jensenemi <jeemi@ucsd.edu>
2025-10-14 13:35:27 -0700

    step 2

--- global.js
+++ global.js
@@ -3,3 +3,16 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+const navLinks = $$('nav a')
+navLinks.forEach(link => {
+    link.classList.remove('current');
+});
+
+let currentLink = navLinks.find(
+    (a) => a.host === location.host && a.pathname === location.pathname,
+  );
+
+if (currentLink) {
+    currentLink.classList.add('current');
+}

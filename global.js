0e136cee2ea7a0956bdcb5dbb2026339e7a13040
jensenemi <jeemi@ucsd.edu>
2025-10-14 14:45:39 -0700

    Update global.js

--- global.js
+++ global.js
@@ -31,25 +31,17 @@ let pages = [
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
-const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
-? "/"                  // Local server
-: "/website/";         // GitHub Pages repo name
-
 for (let p of pages) {
     let url = p.url;
     let title = p.title;
     // next step: create link and add it to nav
-    let a = document.createElement('a');
-    a.href = url;
-    a.textContent = title;
-    if (a.host === location.host && a.pathname === location.pathname) {
-        a.classList.add('current');
-      }
-    if (a.host !== location.host) {
-        a.target = '_blank';
-      }
-    nav.append(a);
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
   }
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+? "/"                  // Local server
+: "/website/";         // GitHub Pages repo name
+  
 
 // if (!url.startsWith('http')) {
 //     url = !url.startsWith('http') ? BASE_PATH + url : url;

fbb19f503c7718116c937cdfce8be59e7562baf4
jensenemi <jeemi@ucsd.edu>
2025-10-22 16:50:50 -0700

    step 3

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-import { fetchJSON, renderProjects } from './global.js';
+import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
 (async () => {
     const projects = await fetchJSON('./lib/projects.json');
     const latestProjects = projects.slice(0, 3);
     const projectsContainer = document.querySelector('.projects');
+    const githubData = await fetchGitHubData('jensenemi');
+    const profileStats = document.querySelector('#profile-stats');
+    if (profileStats) {
+        profileStats.innerHTML = `
+              <dl>
+                <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+                <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+                <dt>Followers:</dt><dd>${githubData.followers}</dd>
+                <dt>Following:</dt><dd>${githubData.following}</dd>
+              </dl>
+          `;
+      }
     renderProjects(latestProjects, projectsContainer, 'h2');
 })();

d6a8619cf62f6b72039930368dd1d9fa0f43de2b
jensenemi <jeemi@ucsd.edu>
2025-10-22 16:38:32 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { fetchJSON, renderProjects, fetchGithubData } from './global.js';
+import { fetchJSON, renderProjects } from './global.js';
 (async () => {
     const projects = await fetchJSON('./lib/projects.json');
     const latestProjects = projects.slice(0, 3);

c948f87d68530f96740b5cbd59d83ae3ec06a76e
jensenemi <jeemi@ucsd.edu>
2025-10-22 16:14:58 -0700

    1.5

--- projects/projects.js
+++ projects/projects.js
@@ -3,5 +3,5 @@ import { fetchJSON, renderProjects } from '../global.js';
 (async () => {
     const projects = await fetchJSON('../lib/projects.json');
     const projectsContainer = document.querySelector('.projects');
-    renderProjects(projects, projectsContainer, 'h4');
+    renderProjects(projects, projectsContainer, 'h2');
   })();

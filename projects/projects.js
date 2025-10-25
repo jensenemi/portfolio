da54759520656124d899235face687cb3a58f272
jensenemi <jeemi@ucsd.edu>
2025-10-25 16:24:37 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -2,7 +2,7 @@ import { fetchJSON, renderProjects } from '../global.js';
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 (async () => {
-    const projects = await fetchJSON('../lib/projects.json');
+    // const projects = await fetchJSON('../lib/projects.json');
     const projectsContainer = document.querySelector('.projects');
     renderProjects(projects, projectsContainer, 'h2');
     const projectsTitle = document.querySelector('.projects-title');
@@ -10,22 +10,17 @@ import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
         projectsTitle.textContent = `${projects.length} Projects`;
     }
   })();
+let projects = await d3.json('projects.json');
+let rolledData = d3.rollups(
+    projects,
+    (v) => v.length,
+    (d) => d.year,
+);
+let data = rolledData.map(([year, count]) => {
+    return { value: count, label: year };
+});
 
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-// let arc = arcGenerator({
-//     startAngle: 0,
-//     endAngle: 2 * Math.PI,
-//   });
-// d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
-
-// let data = [
-//     { value: 1, label: 'apples' },
-//     { value: 2, label: 'oranges' },
-//     { value: 3, label: 'mangos' },
-//     { value: 4, label: 'pears' },
-//     { value: 5, label: 'limes' },
-//     { value: 5, label: 'cherries' },
-// ];
 let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
@@ -43,13 +38,3 @@ data.forEach((d, idx) => {
     .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
     .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
 });
-
-let projects = await d3.json('projects.json');
-let rolledData = d3.rollups(
-  projects,
-  (v) => v.length,
-  (d) => d.year,
-);
-let data = rolledData.map(([year, count]) => {
-    return { value: count, label: year };
-  });

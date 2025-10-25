e9c9971eff4661369b742be17dd4d88e276f3bfb
jensenemi <jeemi@ucsd.edu>
2025-10-25 16:27:11 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -2,7 +2,7 @@ import { fetchJSON, renderProjects } from '../global.js';
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 (async () => {
-    // const projects = await fetchJSON('../lib/projects.json');
+    const projects = await fetchJSON('../lib/projects.json');
     const projectsContainer = document.querySelector('.projects');
     renderProjects(projects, projectsContainer, 'h2');
     const projectsTitle = document.querySelector('.projects-title');
@@ -10,7 +10,7 @@ import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
         projectsTitle.textContent = `${projects.length} Projects`;
     }
   })();
-let projects = await d3.json('projects.json');
+
 let rolledData = d3.rollups(
     projects,
     (v) => v.length,

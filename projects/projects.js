ef3862124189a1258e0837ec3d82643d067cb0a6
jensenemi <jeemi@ucsd.edu>
2025-10-25 16:38:29 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -17,23 +17,23 @@ import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
     let data = rolledData.map(([year, count]) => {
         return { value: count, label: year };
     });
-})();
 
-let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-let sliceGenerator = d3.pie().value((d) => d.value);
-let arcData = sliceGenerator(data);
-let arcs = arcData.map((d) => arcGenerator(d));
+    let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+    let sliceGenerator = d3.pie().value((d) => d.value);
+    let arcData = sliceGenerator(data);
+    let arcs = arcData.map((d) => arcGenerator(d));
 
-let colors = d3.scaleOrdinal(d3.schemeTableau10);
-arcs.forEach((arc, idx) => {
+    let colors = d3.scaleOrdinal(d3.schemeTableau10);
+    arcs.forEach((arc, idx) => {
     d3.select('svg').append('path').attr('d', arc).attr('fill', colors(idx));
 });
 
-let legend = d3.select('.legend');
-data.forEach((d, idx) => {
-  legend
+    let legend = d3.select('.legend');
+    data.forEach((d, idx) => {
+    legend
     .append('li')
     .attr('class', 'legend-item') 
     .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
     .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
-});
+        });
+})();

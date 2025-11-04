a946c63cc26b3f3563bd2a53cd5a59368d94e741
jensenemi <jeemi@ucsd.edu>
2025-11-04 14:02:27 -0800

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -137,9 +137,10 @@ function renderScatterPlot(data, commits) {
 function renderTooltipContent(commit) {
     const link = document.getElementById('commit-link');
     const date = document.getElementById('commit-date');
-  
+    const time = document.getElementById('commit-time');
+    const author = document.getElementById('commit-author');
+    const lines = document.getElementById('commit-lines');
     if (Object.keys(commit).length === 0) return;
-  
     link.href = commit.url;
     link.textContent = commit.id;
     date.textContent = commit.datetime?.toLocaleString('en', {dateStyle: 'full'});

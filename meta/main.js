d8b218f3af3c20bfea3e52aa6ffff440cba98c38
jensenemi <jeemi@ucsd.edu>
2025-11-04 00:24:25 -0800

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -62,10 +62,38 @@ function renderCommitInfo(data, commits) {
     const maxDepth = d3.max(data, d => d.depth);
     dl.append('dt').text('Maximum depth');
     dl.append('dd').text(maxDepth);
-  }
+}
+
+function renderScatterPlot(data, commits) {
+    // Put all the JS code of Steps inside this function
+    const width = 1000;
+    const height = 600;
+    const svg = d3
+        .select('#chart')
+        .append('svg')
+        .attr('viewBox', `0 0 ${width} ${height}`)
+        .style('overflow', 'visible');
+    const xScale = d3
+        .scaleTime()
+        .domain(d3.extent(commits, (d) => d.datetime))
+        .range([0, width])
+        .nice();
+    const yScale = d3.scaleLinear().domain([0, 24]).range([height, 0]);
+    const dots = svg.append('g').attr('class', 'dots');
+    dots
+        .selectAll('circle')
+        .data(commits)
+        .join('circle')
+        .attr('cx', (d) => xScale(d.datetime))
+        .attr('cy', (d) => yScale(d.hourFrac))
+        .attr('r', 5)
+        .attr('fill', 'steelblue');
+
+}
+  
 
 let data = await loadData();
 let commits = processCommits(data);
 
-
 renderCommitInfo(data, commits);
+renderScatterPlot(data, commits);

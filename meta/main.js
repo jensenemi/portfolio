44a42a4453733faa7be563839db86f6e156c8481
jensenemi <jeemi@ucsd.edu>
2025-11-16 23:29:40 -0800

    2.3

--- meta/main.js
+++ meta/main.js
@@ -340,10 +340,11 @@ function updateFileDisplay(filteredCommits) {
   // after initializing filteredCommits
   let lines = filteredCommits.flatMap((d) => d.lines);
   let files = d3
-    .groups(lines, (d) => d.file)
-    .map(([name, lines]) => {
-      return { name, lines };
-  });
+  .groups(lines, (d) => d.file)
+  .map(([name, lines]) => {
+    return { name, lines };
+  })
+  .sort((a, b) => b.lines.length - a.lines.length);
 
   let filesContainer = d3
   .select('#files')
